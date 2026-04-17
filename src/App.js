e3eb82c7a7d3eb4c160fd468857dfa57718d7f8a
Edwin Alexander Vargas <edwinalexandervargas07@gmail.com>
2026-04-17 14:50:44 -0700

    books ratings 5

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Nav from './components/Nav';
 import Landing from './components/Landing';
 import Highlights from './components/Highlights';
+import Featured from './components/Featured';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Landing />
       <Highlights />
+      <Featured />
     </div>
   );
 }
