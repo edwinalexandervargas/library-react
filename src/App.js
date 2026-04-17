522f49a5d3857dadb24e3d35a74742215602d324
Edwin Alexander Vargas <edwinalexandervargas07@gmail.com>
2026-04-17 11:22:09 -0700

    highlights complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './components/Nav';
 import Landing from './components/Landing';
+import Highlights from './components/Highlights';
 
 function App() {
   return (
     <div>
       <Nav />
       <Landing />
+      <Highlights />
     </div>
   );
 }
